64427fb11833bc88d5615977e08e3a96209cd7dd
Dreven95 <104724230+Dreven95@users.noreply.github.com>
2023-03-05 14:34:14 +0300

    homework1

---
+++ src/com/dreven95/homeworks/homework1/Numbers.java
@@ -0,0 +1,101 @@
+package com.dreven95.homeworks.homework1;
+
+public class Numbers {
+    public static void main(String[] args) {
+        //Exercise 1
+        {
+            float fl1 = 3.14F; float fl2 = 42.0F;
+
+            double db1 = fl1 + fl2;
+            System.out.println(db1);
+        }
+
+
+        //Exercise 2
+        {
+            int numberOne = 13;
+            int numberTwo = 12;
+            int result = numberOne / numberTwo;
+            int remainder = numberOne % numberTwo;
+
+            System.out.println("При делении " + numberOne + " на " + numberTwo +
+                    " результат равен " + result + ", остаток равен " + remainder);
+        }
+
+        {
+            System.out.println(signChange(-5)); // Вызов метода из Exercise 3.1
+
+            System.out.println(sumOfNumbers(5)); // Вызов метода из Exercise 3.2
+        }
+
+
+        {
+            //Exercise 4.1
+            int dayOfBirth = 2;
+            int monthOfBirth = 12;
+            int yearOfBirth = 2001;
+
+            //Exercise 4.2
+            int currentDay = 5;
+            int currentMonth = 3;
+            int currentYear = 2023;
+
+            final int MONTH_IN_YEAR = 12;
+            final int DAYS_IN_MONTH = 30;
+            final int SECONDS_IN_DAY = 8400;
+
+            int numberOfYearsPassed = currentYear - yearOfBirth;
+            int numberOfMonthsPassed = (numberOfYearsPassed * MONTH_IN_YEAR) + (currentMonth - monthOfBirth);
+            int numberOfDaysPassed = (numberOfMonthsPassed * DAYS_IN_MONTH) + (currentDay - dayOfBirth);
+            int numberOfSecondsPassed = numberOfDaysPassed * SECONDS_IN_DAY;
+
+            //Exercise 4.3
+            System.out.println("Со дня моего рождения прошло");
+            System.out.println("В годах: " + numberOfYearsPassed);
+            System.out.println("В месяцах: " + numberOfMonthsPassed);
+            System.out.println("В днях: " + numberOfDaysPassed);
+            System.out.println("В секундах: " + numberOfSecondsPassed);
+
+            if (1 <= monthOfBirth && monthOfBirth <= 3) {
+                System.out.println("Я родился в первом квартале");
+            }
+            else if (4 <= monthOfBirth && monthOfBirth <= 6) {
+                System.out.println("Я родился во втором квартале");
+            }
+            else if (7 <= monthOfBirth && monthOfBirth <= 9) {
+                System.out.println("Я родился в третьем квартале");
+            }
+            else if (10 <= monthOfBirth && monthOfBirth <= 12) {
+                System.out.println("Я родился в четвертом квартале");
+            }
+            else {
+                System.out.println("Месяц введен некорректно");
+            }
+
+        }
+
+
+
+    }
+
+    //Exercise 3.1
+    public static int signChange(int number) {
+
+        return -number;
+
+    }
+
+    //Exercise 3.2
+    public static int sumOfNumbers(int number) {
+
+        int i = 0;
+        int result = 0;
+
+        while(i < number) {
+            i++;
+            result += i;
+        }
+
+        return result;
+    }
+}
---
+++ src/com/dreven95/homeworks/homework1/Strings.java
@@ -0,0 +1,115 @@
+package com.dreven95.homeworks.homework1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Strings {
+
+    public static void main(String[] args) throws IOException {
+        //Ex 1
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        {
+            System.out.println("Введите строку: ");
+            String s = reader.readLine();
+            String result = s.substring(0, 1).toUpperCase() + s.substring(1);
+            System.out.println(result);
+        }
+        //Ex 1.2
+        {
+            System.out.println("Введите строку: ");
+            String s = reader.readLine();
+            int counter = 0;
+            char[] chars = s.toCharArray();
+            for (char element : chars) {
+                if (element == ' ') {
+                    counter++;
+                }
+            }
+            System.out.println(counter);
+        }
+        //Ex 2
+        {
+            String history = "History is always written by the winners!" +
+                    " When two cultures clash, the loser is obliterated," +
+                    " and the winner writes the history books - books which glorify their own cause and disparage the conquered foe." +
+                    " As Napoleon once said, 'What is history, but a fable agreed upon?'";
+            String[] strArr = history.split("\\s+");
+            System.out.println(strArr.length);
+            char[] chars = history.toCharArray();
+            int numberOfChars = 0;
+            for (char element: chars) {
+                numberOfChars++;
+            }
+            System.out.println(numberOfChars);
+            int numberOfCharsWithoutSpaces = 0;
+            for (char element: chars) {
+                if (element == ' ') {
+                    continue;
+                }
+                numberOfChars++;
+            }
+            System.out.println(numberOfCharsWithoutSpaces);
+        }
+
+        {
+            countLetters("aaaaaAAAAaaaAaAaA");
+            System.out.println(isPalindrome("казак"));
+            alternationOfUppercaseAndLowercaseLetters("Привет, Мир!");
+        }
+
+    }
+    //Ex 3
+    public static void countLetters(String str) {
+        int lowerCount = 0;
+        int upperCount = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            //Первое условие это проверка является буква английской или нет
+            if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+                if (Character.isLowerCase(c)) {
+                    lowerCount++;
+                } else if (Character.isUpperCase(c)) {
+                    upperCount++;
+                }
+            }
+        }
+
+        System.out.println("Количество строчных букв: " + lowerCount);
+        System.out.println("Количество заглавных букв: " + upperCount);
+    }
+
+    //Ex 4
+    // Тут можно было заморочиться и решить по-другому, на каждой итерации цикла мы каждый раз создаём
+    // новый объект класса String
+    public static boolean isPalindrome(String str) {
+        char[] chars = str.toCharArray();
+        String reverseStr = "";
+        for (int i = chars.length - 1; i >= 0 ; i--) {
+            reverseStr += chars[i];
+        }
+        if (str.equals(reverseStr)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    //Ex 5
+    public static void alternationOfUppercaseAndLowercaseLetters(String str) {
+        char[] chars = str.toCharArray();
+        String result = "";
+        for (int i = 0; i < chars.length; i++) {
+            if (i % 2 == 0) {
+                result += String.valueOf(chars[i]).toUpperCase();
+            }
+            else {
+                result += String.valueOf(chars[i]).toLowerCase();
+            }
+        }
+        System.out.println(result);
+    }
+
+}
